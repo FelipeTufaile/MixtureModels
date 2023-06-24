fb4511a95256c5621b9708b6b2dac8df00ab506f
felipetufaile <f.tufaile@gmail.com>
2023-06-24 15:27:20 -0300

    Updated em.py

--- MixtureModels/em.py
+++ MixtureModels/em.py
@@ -9,35 +9,87 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
     """E-step: Softly assigns each datapoint to a gaussian component
 
     Args:
-        X: (n, d) array holding the data, with incomplete entries (set to 0)
+        X: (n, d) array holding the data
         mixture: the current gaussian mixture
 
     Returns:
         np.ndarray: (n, K) array holding the soft counts
             for all components for all examples
         float: log-likelihood of the assignment
-
     """
-    raise NotImplementedError
+    
+    ## RETURN GAUSSIAN PROBABILITY
+    def gaussian(mu, sigma2, d, x):
+        p = (1/((2*np.pi*sigma2)**(d/2)))*np.exp(-(1/(2*sigma2))*np.linalg.norm(mu-x)**2)
+        return p
+
+    ## GAUSSIAN MIXTURE PROPERTIES
+    #GMM = mixture[0]
+
+    n = len(X) #dataset size
+    k = mixture.mu.shape[0] #number of clusters
+    d = mixture.mu.shape[1] #data dimension R^d
+    post = [] #soft counts
+    l_theta = 0 #log_likelihood
+
+    for i in range(0, n): #over all the datapoints
+
+        p_x_theta_ks = [] #list of individual likelihoods
+        p_x_theta = 0 #sum of likelihoods
+
+        for j in range(0, k): #over all the clusters
+
+            #Calculating the likelihood for each data point regarding to each cluster
+            p_x_theta_k = mixture.p[j]*gaussian(mixture.mu[j], mixture.var[j], d, X[i])
+
+            #Updating 'p_x_theta_k' list
+            p_x_theta_ks.append(p_x_theta_k) 
+
+            #Calculating total likelihood for each data point
+            p_x_theta += p_x_theta_k
+
+        #Calculating the likelihood for each cluster
+        likelihoods = p_x_theta_ks/p_x_theta
+        
+        #log-likelihood update
+        l_theta += np.log(p_x_theta)
+
+        #Assigning X[i] to the cluster where it has the greatest likelihood
+        post.append(likelihoods.tolist())
+               
+    return np.array(post), l_theta 
 
 
 
-def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
-          min_variance: float = .25) -> GaussianMixture:
+def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     """M-step: Updates the gaussian mixture by maximizing the log-likelihood
     of the weighted dataset
 
     Args:
-        X: (n, d) array holding the data, with incomplete entries (set to 0)
+        X: (n, d) array holding the data
         post: (n, K) array holding the soft counts
             for all components for all examples
-        mixture: the current gaussian mixture
-        min_variance: the minimum variance for each gaussian
 
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    
+    n, d = X.shape
+    _, K = post.shape
+
+    n_hat = post.sum(axis=0)
+    p = n_hat / n
+
+    mu = np.zeros((K, d))
+    var = np.zeros(K)
+
+    for j in range(K):
+        mu[j, :] = post[:, j] @ X / n_hat[j]
+        sse = ((mu[j] - X)**2).sum(axis=1) @ post[:, j]
+        var[j] = sse / (d * n_hat[j])
+        
+    return GaussianMixture(mu, var, p)
+
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -55,17 +107,12 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    
+    prev_l_theta = None
+    l_theta = None
+    while (prev_l_theta is None or abs(prev_l_theta - l_theta) > abs(l_theta)*10**(-6)):
+        prev_l_theta = l_theta
+        post, l_theta = estep(X, mixture)
+        mixture = mstep(X, post)
 
-
-def fill_matrix(X: np.ndarray, mixture: GaussianMixture) -> np.ndarray:
-    """Fills an incomplete matrix according to a mixture model
-
-    Args:
-        X: (n, d) array of incomplete data (incomplete entries =0)
-        mixture: a mixture of gaussians
-
-    Returns
-        np.ndarray: a (n, d) array with completed data
-    """
-    raise NotImplementedError
+    return mixture, post, l_theta
