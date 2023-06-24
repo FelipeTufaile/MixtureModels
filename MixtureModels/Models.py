2ae90e73ebf0d7b26430e19ffb8016346090ee21
felipetufaile <f.tufaile@gmail.com>
2023-06-24 15:44:43 -0300

    Updated Models.py

--- MixtureModels/Models.py
+++ MixtureModels/Models.py
@@ -11,4 +11,32 @@ class GaussianMixture(NamedTuple):
   var: np.ndarray  # (K, ) array - each row corresponds to the variance of a component
   p: np.ndarray  # (K, ) array = each row corresponds to the weight of a component
 
-  f
+  def init(X: np.ndarray, K: int, seed: int = 0) -> Tuple[GaussianMixture, np.ndarray]:
+    """Initializes the mixture model with random points as initial
+    means and uniform assingments
+
+    Args:
+        X: (n, d) array holding the data
+        K: number of components
+        seed: random seed
+
+    Returns:
+        mixture: the initialized gaussian mixture
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+    """
+    np.random.seed(seed)
+    n, _ = X.shape
+    p = np.ones(K) / K
+
+    # select K random points as initial means
+    mu = X[np.random.choice(n, K, replace=False)]
+    var = np.zeros(K)
+    # Compute variance
+    for j in range(K):
+        var[j] = ((X - mu[j])**2).mean()
+
+    mixture = GaussianMixture(mu, var, p)
+    post = np.ones((n, K)) / K
+
+    return mixture, post
